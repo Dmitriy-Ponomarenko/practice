c4a6a5386174da0df6a4602c3cdfe46f4491e8d2
Yumfii <144070576+Yumfii@users.noreply.github.com>
2024-07-10 20:01:53 +0200

    hack

--- my-script.js
+++ my-script.js
@@ -4,7 +4,30 @@ const button = document.getElementById('heroButton');
 
 button.addEventListener('click', function() {
 
-    alert("The result was written in your console");
+    alert("You computer was hacked. The result was written in your console");
+    alert("You computer was hacked. The result was written in your console");
+    alert("You computer was hacked. The result was written in your console");
+    alert("You computer was hacked. The result was written in your console");
+    alert("You computer was hacked. The result was written in your console");
+    alert("You computer was hacked. The result was written in your console");
+    alert("You computer was hacked. The result was written in your console");
+    alert("You computer was hacked. The result was written in your console");
+    alert("You computer was hacked. The result was written in your console");
+    alert("You computer was hacked. The result was written in your console");
+    alert("You computer was hacked. The result was written in your console");
+    alert("You computer was hacked. The result was written in your console");
+    alert("You computer was hacked. The result was written in your console");
+    alert("You computer was hacked. The result was written in your console");
+    alert("You computer was hacked. The result was written in your console");
+    alert("You computer was hacked. The result was written in your console");
+    alert("You computer was hacked. The result was written in your console");
+    alert("You computer was hacked. The result was written in your console");
+    alert("You computer was hacked. The result was written in your console");
+    alert("You computer was hacked. The result was written in your console");
+    alert("You computer was hacked. The result was written in your console");
+    alert("You computer was hacked. The result was written in your console");
+    alert("You computer was hacked. The result was written in your console");
+    alert("You computer was hacked. The result was written in your console");
 
     function priceCounter(x, y, z) {
 
