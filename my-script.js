00e1c8fab5b3470b534ab6e998471a7c55b7bce2
Yumfii <144070576+Yumfii@users.noreply.github.com>
2024-07-10 19:49:36 +0200

    Basic function + design

--- my-script.js
+++ my-script.js
@@ -3,5 +3,25 @@
 const button = document.getElementById('heroButton');
 
 button.addEventListener('click', function() {
-    console.log('Кнопка была нажата!');
+
+    // alert("The result was written in your console");
+
+    function priceCounter(x, y, z) {
+
+        return x * y + z;
+    }
+
+    const orderCount = 2;
+    const orderPrice = 800;
+    const deliveryPrice = 50;
+    const finalPrice = priceCounter (orderCount, orderPrice, deliveryPrice);
+    
+    const command = `12.745 You ${orderCount} orders will cost you ${finalPrice} credits`;
+    console.log(command);
+    
+    // const float = Number.parseFloat(command)
+    // console.log(command.length);
+    // console.log(command[command.length - 1]);
+    // console.log(float);
+    // console.log(Math.round(float));
 });
