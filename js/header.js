b1ebfc3fbd25bfed59803f5f4112416e8875a9c3
Yumfii <144070576+Yumfii@users.noreply.github.com>
2024-07-12 01:58:49 +0200

    commented

--- js/header.js
+++ js/header.js
@@ -1,15 +1,30 @@
-document.querySelectorAll('.accordion-item__trigger').forEach((item) =>
-    item.addEventListener('click', () => {
-        const parent = item.parentNode;
+document.querySelectorAll(".accordion-item__trigger").forEach((item) =>
+  item.addEventListener("click", () => {
+    const parent = item.parentNode;
 
-        if(parent.classList.contains('accordion-item--active')) {
-            parent.classList.remove('accordion-item--active');
-        } else {
-            document
-            .querySelectorAll('.accordion-item')
-            .forEach((child) => child.classList.remove('accordion-item--active'))
+    if (parent.classList.contains("accordion-item--active")) {
+      parent.classList.remove("accordion-item--active");
+    } else {
+      document
+        .querySelectorAll(".accordion-item")
+        .forEach((child) => child.classList.remove("accordion-item--active"));
 
-        parent.classList.add('accordion-item--active');
-        }
-    })
-)
+      parent.classList.add("accordion-item--active");
+    }
+  })
+);
+
+// Теория: Нужно двигать весь контейнер аккордеона, а не только ссылки, а его заголовок делать невидимым + негативный маржин
+
+// window.addEventListener('scroll', function() {
+//     var accordionContent = document.querySelector('.accordion-item__content');
+//     var accordionTrigger = document.querySelector('.accordion-item__trigger');
+//     var accordionPosition = accordionContent.getBoundingClientRect().top - 20;
+//     if (accordionPosition <= 0) {
+//        accordionContent.classList.add('fixed');
+//        accordionTrigger.style.width = '100%';
+//        } else {
+//            accordionContent.classList.remove('fixed');
+//            accordionTrigger.style.width = 'auto';
+//        }
+//     });
--- my-script.js
+++ my-script.js
@@ -1,27 +1,28 @@
-'use sctrict';
+"use sctrict";
 
-const button = document.getElementById('heroButton');
+const button = document.getElementById("heroButton");
 
-button.addEventListener('click', function() {
+button.addEventListener("click", function () {
+  // const a = true;
+  // while (a == true) {
+  //     alert("The result was written in your console");
+  // }
 
-    // alert("The result was written in your console");
+  function priceCounter(x, y, z) {
+    return x * y + z;
+  }
 
-    function priceCounter(x, y, z) {
+  const orderCount = 2;
+  const orderPrice = 800;
+  const deliveryPrice = 50;
+  const finalPrice = priceCounter(orderCount, orderPrice, deliveryPrice);
 
-        return x * y + z;
-    }
+  const command = `12.745 You ${orderCount} orders will cost you ${finalPrice} credits`;
+  console.log(command);
 
-    const orderCount = 2;
-    const orderPrice = 800;
-    const deliveryPrice = 50;
-    const finalPrice = priceCounter (orderCount, orderPrice, deliveryPrice);
-    
-    const command = `12.745 You ${orderCount} orders will cost you ${finalPrice} credits`;
-    console.log(command);
-    
-    // const float = Number.parseFloat(command)
-    // console.log(command.length);
-    // console.log(command[command.length - 1]);
-    // console.log(float);
-    // console.log(Math.round(float));
+  // const float = Number.parseFloat(command)
+  // console.log(command.length);
+  // console.log(command[command.length - 1]);
+  // console.log(float);
+  // console.log(Math.round(float));
 });
