0ccff16f285caa47b7a802036d07dce4c3f27b5e
Yumfii <144070576+Yumfii@users.noreply.github.com>
2024-07-13 15:02:20 +0200

    script update

--- my-script.js
+++ my-script.js
@@ -12,13 +12,43 @@ button.addEventListener("click", function () {
     return x * y + z;
   }
 
+  var balance = 3000;
   const orderCount = 2;
   const orderPrice = 800;
   const deliveryPrice = 50;
   const finalPrice = priceCounter(orderCount, orderPrice, deliveryPrice);
 
   const command = `12.745 You ${orderCount} orders will cost you ${finalPrice} credits`;
-  console.log(command);
+  console.log(command.trim());
+
+
+  
+  // const user = "Kate";
+  // switch (user) {
+  //   case "Kate":
+  //     console.log("Hello " + user + " !");
+  //     break;
+  //   case "Alice":
+  //     console.log("Hello " + user + " !");
+  //     break;
+  //   default:
+  //     console.log("Hello " + "User" + " !");
+  // }
+
+
+  // const message =
+  //   balance >= finalPrice
+  //     ? `12.745 You ${orderCount} orders will cost you ${finalPrice} credits`
+  //     : "You don't have enough money";
+
+  //     console.log(message);
+
+  // if(balance >= finalPrice) {
+  //   const command = `12.745 You ${orderCount} orders will cost you ${finalPrice} credits`;
+  //   console.log(command);
+  // } else {
+  //   console.log("You don't have enough money");
+  // }
 
   // const float = Number.parseFloat(command)
   // console.log(command.length);
