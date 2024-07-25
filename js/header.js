5d576add98f211051049472a5ea2ccc4fcefc6fc
Yumfii <144070576+Yumfii@users.noreply.github.com>
2024-07-25 17:33:34 +0200

    Update header.js

--- js/header.js
+++ js/header.js
@@ -13,18 +13,3 @@ document.querySelectorAll(".accordion-item__trigger").forEach((item) =>
     }
   })
 );
-
-// Теория: Нужно двигать весь контейнер аккордеона, а не только ссылки, а его заголовок делать невидимым + негативный маржин
-
-// window.addEventListener('scroll', function() {
-//     var accordionContent = document.querySelector('.accordion-item__content');
-//     var accordionTrigger = document.querySelector('.accordion-item__trigger');
-//     var accordionPosition = accordionContent.getBoundingClientRect().top - 20;
-//     if (accordionPosition <= 0) {
-//        accordionContent.classList.add('fixed');
-//        accordionTrigger.style.width = '100%';
-//        } else {
-//            accordionContent.classList.remove('fixed');
-//            accordionTrigger.style.width = 'auto';
-//        }
-//     });
